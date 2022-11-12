ece56cea9d3011b86604503a1629e73938ea0b07
Yash Tripathi <tripathiyash143@gmail.com>
2022-11-12 03:59:10 +0000

    question1(b)

---
+++ assignment2.py
@@ -0,0 +1,12 @@
+#	5	4	3	2	1
+#	4	3	2	1
+#	3	2	1
+#	2	1	
+#	1
+rows = int(input("Enter number of rows: "))
+
+for i in range(rows, 0, -1):
+    for j in range(i,0,-1):
+        print(j, end=" ")
+    
+    print("\n")
